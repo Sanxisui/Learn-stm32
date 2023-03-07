ffaeaef35a0881e590ba491d4a09a1d5755acf01
Sanxisui <sanxisui@gmail.com>
2023-03-08 01:54:17 +0800

    1.我的第一个go语言程序hello，world

---
+++ mycode/new_langauge/helloworld.go
@@ -0,0 +1,12 @@
+/***********************************************************
+File name : tls_server_my.c
+Author name : xiongyansong
+Email address : sanxisui@gmail.com
+Create time : 2023-03-08 20:06:07
+***********************************************************/
+package main
+import "fmt"
+func main(){
+	fmt.Println("hello,world");
+	fmt.Println("This is my first go pragram");
+}
